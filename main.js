a3f6f02630a5c67994c07fd58c9714d30726717d
michele <michele.giovagnoli1@gmail.com>
2023-04-20 16:41:25 +0200

    Aggiunta di una task e stilizzazione

--- main.js
+++ main.js
@@ -3,18 +3,31 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            jsList: [],
-            item: ''
+            jsList: [],     //array 
+            item: "",
         }
     },
     methods: {
-        readList() {
+        readList() {                             //API
             axios.get('server.php')
-                .then(response => {
-                    this.jsList = response.data;
-                })
-        }
+                .then(response => {              //response.data sara il contenuto della chiamata API
+                    this.jsList = response.data; //Metto la risposta alla chiamata all'interno di un'array
+                })                               //facendone una 'copia' che utilizzero con vue.js in index.html
+        },
+        addTask() {
+            const data = {                       //Creo un'oggetto che verra inviato con una post al server
+                task: this.item                  //
+            };
 
+            axios.post('server.php', data,       //scelgo il file e cosa inviare
+                {
+                    headers: { 'Content-Type': 'multipart/form-data' }
+                }
+            ).then(response => {
+                this.jsList = response.data;
+                this.item = "";
+            });
+        },
     },
     mounted() {
         this.readList();
--- server.php
+++ server.php
@@ -1,10 +1,14 @@
-<?php
-    $list = [
+<?php                        //server
+    $list = [               //array 
         'spesa',
         'bollette',
         'assicurazione',
-        'affitto'
+        'affitto',
     ];
+    if (isset($_POST['task'])){
+        $list[] .= $_POST['task'];
+    }
 
-    header('Content-Type: application/json');
-    echo json_encode($list);
+    header('Content-Type: application/json');  // converto i dati che voglio spedire
+    echo json_encode($list);                   // in una stringa di testo contenuta 
+                                               // all'intero di un file .json
